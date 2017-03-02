8ef045ce1135a661bbcc3f2a5f2023672cd8fa90
mgbukov <mgbukov@gmail.com>
2017-03-02 09:07:32 -0500

    adding partial trace test

---
+++ bosons_test.py
@@ -0,0 +1,49 @@
+import sys,os
+qspin_path = os.path.join(os.getcwd(),"../basis_1d_base/")
+sys.path.insert(0,qspin_path)
+
+
+from quspin.operators import hamiltonian # Hamiltonians and operators
+from quspin.basis import boson_basis_1d, spin_basis_1d # Hilbert space spin basis
+import numpy as np # generic math functions
+
+##### define model parameters #####
+L=10 # system size
+
+J=1.0
+h=0.8945
+
+J_zz=[[J,i,(i+1)%L] for i in range(L)] # PBC
+x_field=[[h,i] for i in range(L)]
+hopping=[[0.5*h,i] for i in range(L)]
+chem_pot=[[-J,i] for i in range(L)]
+identity=[[0.25,i] for i in range(L)]
+
+#### define spin model
+basis_spin = spin_basis_1d(L=L,pauli=False,a=1,kblock=0,pblock=1)
+
+static_spin =[["zz",J_zz],["x",x_field]]
+#static_spin =[["z",x_field]]
+
+H_spin=hamiltonian(static_spin,[],basis=basis_spin,dtype=np.float32)
+E_spin=H_spin.eigvalsh()
+
+#### define hcb model
+basis_boson = boson_basis_1d(L=L,sps=2,a=1,kblock=0,pblock=1)
+
+static_boson =[["+",hopping],["-",hopping],["n",chem_pot],["nn",J_zz],["I",identity]]
+#static_boson =[["n",chem_pot],["I",identity]]
+
+H_boson=hamiltonian(static_boson,[],basis=basis_boson,dtype=np.float32)
+E_boson=H_boson.eigvalsh()
+
+
+#print(max(abs(E_boson-E_spin)))
+
+#### 
+
+np.testing.assert_allclose(E_boson-E_spin,0.0,atol=1E-5,err_msg='Failed boson and ho energies comparison!')
+
+
+
+
---
+++ part_trace.py
@@ -0,0 +1,79 @@
+import sys,os
+qspin_path = os.path.join(os.getcwd(),"../basis_1d_base/")
+sys.path.insert(0,qspin_path)
+
+import numpy as np
+
+from quspin.operators import hamiltonian
+from quspin.basis import boson_basis_1d
+from quspin.tools.measurements import ent_entropy
+#from memory_profiler import profile
+
+#@profile
+def partial_trace(rho,sub_sys_A,L,sps=2):
+	rho = np.atleast_2d(rho)
+
+	if rho.shape[0] != rho.shape[1]:
+		raise ValueError("expecting square matrix")
+
+	sub_sys_A = set(sub_sys_A)
+	sub_sys_B = set(range(L))-sub_sys_A
+
+	Ns_B = (sps**len(sub_sys_B))
+	Ns_A = (sps**len(sub_sys_A))
+
+	# T_tup tells numpy how to reshuffle the indices such that when I reshape the array to the 
+	# 4-Tensor rho_{ik,jl} i,j are for sub_sys_B and k,l are for sub_sys_A
+	# which means I need (sub_sys_B,sub_sys_A,sub_sys_B+L,sub_sys_A+L)
+
+	T_tup = tuple(sub_sys_B)+tuple(sub_sys_A) # 
+	T_tup += tuple(s+L for s in T_tup)
+
+	print("The following line need to be modified for symemtries")
+	rho_v = rho.reshape(tuple(sps for i in range(2*L))) # DM where index is given per site as rho_v[i_1,...,i_L,j_1,...j_L]
+	rho_v = rho_v.transpose(T_tup) # take transpose to reshuffle indices
+	rho_v = rho_v.reshape((Ns_B,Ns_A,Ns_B,Ns_A)) 
+
+	return np.einsum("ijik->jk",rho_v)
+
+
+
+##### define model parameters #####
+L=6 # system size
+
+J=1.0 # hopping
+U=np.sqrt(2) # interactions strenth
+
+
+# define site-coupling lists
+interaction=[[U/2.0,i,i] for i in range(L)] # PBC
+chem_pot=[[-U/2.0,i] for i in range(L)] # PBC
+hopping=[[J,i,(i+1)%L] for i in range(L)] # PBC
+
+#### define hcb model
+basis = boson_basis_1d(L=L,sps=3)
+
+
+# Hubbard-related model
+static =[["+-",hopping],["-+",hopping],["n",chem_pot],["nn",interaction]]
+
+H=hamiltonian(static,[],basis=basis,dtype=np.float32)
+E,V=H.eigh()
+
+psi=V[:,0]
+chain_subsys=[0,4]
+
+DM=ent_entropy(psi,basis,DM='chain_subsys',chain_subsys=chain_subsys)['DM_chain_subsys']
+DM_pt = partial_trace( np.outer(psi.conj(),psi),chain_subsys,L,basis.sps )
+
+
+np.testing.assert_allclose(DM-DM_pt,0.0,atol=1E-5,err_msg='Failed test partial trace!')
+
+
+
+
+
+
+
+
+
